13a8c7c1b8514f6e12b7a9a39308e23eee655aca
Konrad Chlupka <chlupkaw@gmail.com>
2019-09-10 18:02:14 +0100

    Rearrange code

--- optimization.py
+++ optimization.py
@@ -1,11 +1,100 @@
 import random
-import numpy as np
 import time
+
+import numpy as np
 import matplotlib.pyplot as plt
 from deap import base  # contains Toolbox and base Fitness
 from deap import creator  # creating types
 from deap import tools  # contains operators
 from deap import algorithms  # contains ready genetic evolutionary loops
+from scipy import signal
+
+SIGNAL_TIMEBASE = 20e-9  # one period
+SIGNAL_LEN = 240  # one period has 240 points
+STEADY_NEGATIVE_LEN = 110  # each period starts with a const -1
+FULL_RANGE_LEN = 30  # in the rising part, signal can be from -1 to 1
+TOP_LEN = 100  # final part of the signal can be between 0.5 and 1
+EVOLVING_LEN = 130  # only last 130 points are subject to evolution
+
+# system
+num = [2.01199757841099e115]
+den = [
+    1,
+    1.00000001648985e19,
+    1.64898505756825e30,
+    4.56217233166632e40,
+    3.04864287973918e51,
+    4.76302109455371e61,
+    1.70110870487715e72,
+    1.36694076792557e82,
+    2.81558045148153e92,
+    9.16930673102975e101,
+    1.68628748250276e111,
+    2.40236028415562e120,
+]
+trans_func = signal.TransferFunction(num, den)
+
+
+
+def initial_state(trans_func):
+    """TODO: docu
+    """
+    U = np.array([-1.0] * 240)
+    T = np.linspace(0, 20e-9, 240)
+    (T, yout, xout) = signal.lsim2(
+        trans_func, U=np.array([-1.0] * 240), T=T, X0=None, atol=1e-21
+    )
+    return xout[-1]
+
+
+def rise_time(T, yout):
+    """TODO: docu
+    """
+    ss = np.mean(yout[-24:])  # steady-state
+    start = yout[0]
+    min_to_ss = ss - start
+    ss_90 = start + 0.9 * min_to_ss
+    ss_10 = start + 0.1 * min_to_ss
+    return (
+        T[next(i for i in range(len(yout) - 1) if yout[i] > ss_90)]
+        - T[next(i for i in range(len(yout) - 1) if yout[i] > ss_10)]
+    )
+
+
+def mean_squared_error(T, yout):
+    """TODO: docu
+    """
+    ss = np.mean(yout[-24:])  # steady-state
+    start = yout[0]
+    perfect_response = np.array([start] * 120 + [ss] * 120)
+    return np.mean((perfect_response - yout) ** 2)
+
+
+def illegal_driver_signal(U):
+    """TODO: docu
+    """
+    if any(i != -1.0 for i in U[:STEADY_NEGATIVE_LEN]):
+        raise AssertionError("First part of signal cannot change")
+    # fmt: off
+    return (
+        any(i < -1.0 for i in U[STEADY_NEGATIVE_LEN:STEADY_NEGATIVE_LEN + FULL_RANGE_LEN])
+        or any(i > 1.0 for i in U[STEADY_NEGATIVE_LEN:STEADY_NEGATIVE_LEN + FULL_RANGE_LEN])
+        or any(i < 0.5 for i in U[STEADY_NEGATIVE_LEN + FULL_RANGE_LEN:])
+        or any(i > 1.0 for i in U[STEADY_NEGATIVE_LEN + FULL_RANGE_LEN:])
+    )
+    # fmt: on
+
+
+U = np.array([-1.0] * 120 + [1.0] * 120)
+T = np.linspace(0, 20e-9, 240)
+X0 = initial_state(trans_func)
+
+# default rtol value is fine, but atol needs to be below 1e-21 or lower
+(_, yout, _) = signal.lsim2(trans_func, U=U, T=T, X0=X0, atol=1e-21)
+plt.plot(T, yout)
+input()
+
+
 
 creator.create(
     "FitnessMax", base.Fitness, weights=(1.0,)
@@ -80,91 +169,3 @@ if __name__ == "__main__":
     input()
 
 
-# signal code
-import matplotlib.pyplot as plt
-from scipy import signal
-import numpy as np
-
-# system
-num = [2.01199757841099e115]
-den = [
-    1,
-    1.00000001648985e19,
-    1.64898505756825e30,
-    4.56217233166632e40,
-    3.04864287973918e51,
-    4.76302109455371e61,
-    1.70110870487715e72,
-    1.36694076792557e82,
-    2.81558045148153e92,
-    9.16930673102975e101,
-    1.68628748250276e111,
-    2.40236028415562e120,
-]
-trans_func = signal.TransferFunction(num, den)
-
-SIGNAL_TIMEBASE = 20e-9  # one period
-SIGNAL_LEN = 240  # one period has 240 points
-STEADY_NEGATIVE_LEN = 110  # each period starts with a const -1
-FULL_RANGE_LEN = 30  # in the rising part, signal can be from -1 to 1
-TOP_LEN = 100  # final part of the signal can be between 0.5 and 1
-EVOLVING_LEN = 130  # only last 130 points are subject to evolution
-
-
-def initial_state(trans_func):
-    """TODO: docu
-    """
-    U = np.array([-1.0] * 240)
-    T = np.linspace(0, 20e-9, 240)
-    (T, yout, xout) = signal.lsim2(
-        trans_func, U=np.array([-1.0] * 240), T=T, X0=None, atol=1e-21
-    )
-    return xout[-1]
-
-
-def rise_time(T, yout):
-    """TODO: docu
-    """
-    ss = np.mean(yout[-24:])  # steady-state
-    start = yout[0]
-    min_to_ss = ss - start
-    ss_90 = start + 0.9 * min_to_ss
-    ss_10 = start + 0.1 * min_to_ss
-    return (
-        T[next(i for i in range(len(yout) - 1) if yout[i] > ss_90)]
-        - T[next(i for i in range(len(yout) - 1) if yout[i] > ss_10)]
-    )
-
-
-def mean_squared_error(T, yout):
-    """TODO: docu
-    """
-    ss = np.mean(yout[-24:])  # steady-state
-    start = yout[0]
-    perfect_response = np.array([start] * 120 + [ss] * 120)
-    return np.mean((perfect_response - yout) ** 2)
-
-
-def illegal_driver_signal(U):
-    """TODO: docu
-    """
-    if any(i != -1.0 for i in U[:STEADY_NEGATIVE_LEN]):
-        raise AssertionError("First part of signal cannot change")
-    # fmt: off
-    return (
-        any(i < -1.0 for i in U[STEADY_NEGATIVE_LEN:STEADY_NEGATIVE_LEN + FULL_RANGE_LEN])
-        or any(i > 1.0 for i in U[STEADY_NEGATIVE_LEN:STEADY_NEGATIVE_LEN + FULL_RANGE_LEN])
-        or any(i < 0.5 for i in U[STEADY_NEGATIVE_LEN + FULL_RANGE_LEN:])
-        or any(i > 1.0 for i in U[STEADY_NEGATIVE_LEN + FULL_RANGE_LEN:])
-    )
-    # fmt: on
-
-
-U = np.array([-1.0] * 120 + [1.0] * 120)
-T = np.linspace(0, 20e-9, 240)
-X0 = initial_state(trans_func)
-
-# default rtol value is fine, but atol needs to be below 1e-21 or lower
-(_, yout, _) = signal.lsim2(trans_func, U=U, T=T, X0=X0, atol=1e-21)
-plt.plot(T, yout)
-input()
