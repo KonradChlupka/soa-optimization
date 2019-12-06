30652a7341dbc690b6294e6c813a62682eedb53e
Konrad Chlupka <chlupkaw@gmail.com>
2019-12-06 19:23:30 +0000

    Select correct channel

--- analysis_of_averages.py
+++ analysis_of_averages.py
@@ -11,6 +11,5 @@ for n_of_averages in range(1, 101):
     dict_resp["outputs"] = []
     for i in range(100):
         time.sleep(0.5 + n_of_averages / 10)
-        dict_resp["outputs"].append(osc.measurement())
+        dict_resp["outputs"].append(osc.measurement(channel=1))
     results.append(dict_resp)
-
