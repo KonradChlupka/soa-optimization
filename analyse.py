d17e973574c8949d5d5ce6bed952f347322bf16b
Konrad Chlupka <chlupkaw@gmail.com>
2019-12-10 17:32:52 +0000

    Add Zach's original code

--- analyse.py
+++ analyse.py
@@ -1,133 +1,132 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import arange
 
+#Get FOPDT parameters quickly from input (driving signal), output and a time axis.
+#
+#USE EXAMPLE:
+#fopdtParams = FopdtMeasurements(signal,drive,t)
+#-->
+#Kc = fopdtParams.Kc
+#TauP = fopdtParams.TauP
+#ThetaP = fopdtParams.ThetaP
 
 class ResponseMeasurements:
-    def __init__(self, signal, t, gradient_points=8, percentage=5, hop_size=1):
+
+    def __init__(self,signal,t,gradientPoints=20,percentage=5):
+
+        
         self.signal = signal
         self.t = t
-        self.gradient_points = gradient_points
-        self.percentage = percentage / 100
-        self.hop_size = hop_size
-        self.dt = abs(self.t[len(self.t) - 1] - self.t[len(self.t) - 2])
-        self.__get_measurements()
+        self.gradientPoints = gradientPoints
+        self.percentage = percentage/100
 
-    def __get_measurements(self):
-        self.__get_inflection_time_index()
-        self.__get_settling_max_index()
-        self.__get_settling_time_index()
-        self.__get_ss_high_value()
-        self.__get_ss_low_value()
-        self.__get_sp()
-        self.__get_rise_time()
-        self.__get_overshoot()
+        self.dt = abs(self.t[len(self.t)-1] - self.t[len(self.t)-2])
 
-    def __get_sp(self):
-        self.sp = SetPoint(self)
+        self.__getMeasurements()
 
-    def __get_inflection_time_index(self):
+    def __getMeasurements(self):
+
+        self.__getInflectionTimeIndex()
+        self.__getSettlingMaxIndex()
+        self.__getSettlingTimeIndex()
+        self.__getSSHighValue()
+        self.__getSSLowValue()
+        self.__getRiseTime()
+        self.__getOvershoot()
+
+    def __getInflectionTimeIndex(self):
+
         grad = []
         n = len(self.signal)
         for i in range(n):
-            if i + self.gradient_points < n:
-                grad.append(abs(self.signal[i] - self.signal[i + self.gradient_points]))
+            if i + self.gradientPoints < n:
+                grad.append(abs(self.signal[i] - self.signal[i+self.gradientPoints]))
             else:
                 break
+        self.inflectionTimeIndex = np.argmax(grad)
 
-        self.inflection_time_index = np.argmax(grad)
+    def __getSettlingMaxIndex(self):
+        self.settlingMaxIndex = np.argmax(self.signal)
 
-    def __get_settling_max_index(self):
-        self.settling_max_index = np.argmax(self.signal)
-
-    def __get_settling_time_index(self):
-        ss_high = self.signal[self.settling_max_index]
+    def __getSettlingTimeIndex(self):
+        ss_high = self.signal[self.settlingMaxIndex] 
         n = len(self.signal)
-        signal_end = self.signal[len(self.signal) - 10]
+        signal_end = self.signal[len(self.signal)-1]
+
         for i in range(n):
-            if (
-                np.max(abs(self.signal[i:] - signal_end))
-                <= self.percentage * signal_end
-            ):
-                self.settling_time_index = i
-                self.settling_time = (i - self.inflection_time_index) * abs(
-                    self.t[0] - self.t[1]
-                )
+            if np.max(abs(self.signal[i:] - signal_end)) <= self.percentage * signal_end:
+                self.settlingTimeIndex = i
+                self.settlingTime = i*abs(self.t[0] - self.t[1])
                 break
 
-            else:
-                self.settling_time_index = len(self.signal) - 1
-                self.settling_time = 1000  # signal never settles
+    def __getSSHighValue(self):
+        self.SSHighValue = np.mean(self.signal[self.settlingTimeIndex:])
 
-    def __get_ss_high_value(self):
-        self.ss_high_value = np.mean(
-            self.signal[self.settling_time_index : len(self.signal) - 10]
-        )
+    def __getSSLowValue(self):
+        self.SSLowValue = np.mean(self.signal[:self.inflectionTimeIndex])
 
-    def __get_ss_low_value(self):
+    def __getRiseTime(self):
 
-        self.ss_low_value = np.mean(self.signal[: self.inflection_time_index])
-
-    def __get_rise_time(self):
-        off_set = self.sp.sp[0]  # get amount signal is offset from 0 by
-        i = self.inflection_time_index
-        prev_diff = abs(
-            (self.signal.copy()[i] - off_set) - (((self.ss_high_value - off_set) * 0.1))
-        )
-        curr_diff = abs(
-            (self.signal.copy()[i + 1] - off_set)
-            - (((self.ss_high_value - off_set) * 0.1))
-        )
+        i=self.inflectionTimeIndex
+        prev_diff = abs((self.signal.copy()[i]) - (((self.SSHighValue)*0.1)))
+        curr_diff = abs((self.signal.copy()[i+1]) - (((self.SSHighValue)*0.1)))
         while curr_diff < prev_diff:
-            prev_diff = abs(
-                (self.signal.copy()[i] - off_set)
-                - (((self.ss_high_value - off_set) * 0.1))
-            )
-            curr_diff = abs(
-                (self.signal.copy()[i + 1] - off_set)
-                - (((self.ss_high_value - off_set) * 0.1))
-            )
+            prev_diff = abs((self.signal.copy()[i]) - (((self.SSHighValue)*0.1)))
+            curr_diff = abs((self.signal.copy()[i+1]) - (((self.SSHighValue)*0.1)))
             i += 1
-        self.idx_ten = i - 1
+        self.idxTen = i-1
 
-        i = self.inflection_time_index
-        prev_diff = abs(
-            (self.signal.copy()[i] - off_set) - (((self.ss_high_value - off_set) * 0.9))
-        )
-        curr_diff = abs(
-            (self.signal.copy()[i + 1] - off_set)
-            - (((self.ss_high_value - off_set) * 0.9))
-        )
+        i=self.inflectionTimeIndex
+        prev_diff = abs((self.signal.copy()[i]) - (((self.SSHighValue)*0.9)))
+        curr_diff = abs((self.signal.copy()[i+1]) - (((self.SSHighValue)*0.9)))
         while curr_diff < prev_diff:
-            prev_diff = abs(
-                (self.signal.copy()[i] - off_set)
-                - (((self.ss_high_value - off_set) * 0.9))
-            )
-            curr_diff = abs(
-                (self.signal.copy()[i + 1] - off_set)
-                - (((self.ss_high_value - off_set) * 0.9))
-            )
+            prev_diff = abs((self.signal.copy()[i]) - (((self.SSHighValue)*0.9)))
+            curr_diff = abs((self.signal.copy()[i+1]) - (((self.SSHighValue)*0.9)))
             i += 1
-        self.idx_ninety = i - 1
-        time_ten = self.t[self.idx_ten]
-        time_ninety = self.t[self.idx_ninety]
-        self.rise_time = time_ninety - time_ten
+        self.idxNinety = i-1
 
-    def __get_overshoot(self):
-        self.overshoot = abs(
-            float(
-                (self.signal[self.settling_max_index] - self.ss_high_value)
-                / self.ss_high_value
-            )
-        )
 
+        # self.idxTen = np.argmin(np.abs((self.signal.copy() - ((self.SSHighValue)*0.1 + off_set))))
+
+        # self.idxNinety = np.argmin(np.abs(self.signal.copy() - ((self.SSHighValue)*0.9)))
+
+        timeTen = self.t[self.idxTen]
+
+        timeNinety = self.t[self.idxNinety]
+
+        self.riseTime = (timeNinety - timeTen)
+
+        '''        idxTen = np.argmin(np.abs(self.signal.copy() - self.SSHighValue*0.1))
+        idxNinety = np.argmin(np.abs(self.signal.copy() - self.SSHighValue*0.9))
+        timeTen = self.t[idxTen]
+        timeNinety = self.t[idxNinety]
+        self.riseTime = timeNinety - timeTen
+        self.ten = idxTen
+        self.ninety = idxNinety'''
+
+    def __getOvershoot(self):
+        self.overshoot = abs(float((self.signal[self.settlingMaxIndex] - self.SSHighValue)/self.SSHighValue))
 
 class SetPoint:
-    def __init__(self, response):
+
+    def __init__(self,response):
+
         self.response = response
         self.__getSetPoint()
-
+    
     def __getSetPoint(self):
+
         self.sp = np.zeros(len(self.response.signal))
-        self.inflection_time_index = self.response.inflection_time_index
-        self.sp[: self.inflection_time_index] = self.response.ss_low_value
-        self.sp[self.inflection_time_index :] = self.response.ss_high_value
+        self.inflectionTimeIndex = self.response.inflectionTimeIndex
+        self.sp[:self.inflectionTimeIndex] = self.response.SSLowValue
+        self.sp[self.inflectionTimeIndex:] = self.response.SSHighValue
+
+# # TEMPLATE
+# drive = np.genfromtxt('../fopdt_param_test_signals/input.csv')
+# signal = np.genfromtxt('../fopdt_param_test_signals/output.csv')
+# t = np.genfromtxt('../fopdt_param_test_signals/time.csv')
+
+# # test = FopdtMeasurements(signal,drive,t)
+# test = ResponseMeasurements(signal,t)
+# # print(test.ThetaP)
