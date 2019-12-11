070795dece82f3b5576ed92e23d0dc3cd241a519
Konrad Chlupka <chlupkaw@gmail.com>
2019-12-11 12:27:21 +0000

    Significant reorg of simulation_optimization.py

--- soa_optimization/simulation_optimization.py
+++ soa_optimization/simulation_optimization.py
@@ -1,20 +1,19 @@
-import random
-import time
 import multiprocessing
+import pickle
+import random
 import sys
 import threading
-import pickle
+import time
 
 import numpy as np
 import matplotlib.pyplot as plt
+from deap import algorithms  # contains ready genetic evolutionary loops
 from deap import base  # contains Toolbox and base Fitness
 from deap import creator  # creating types
 from deap import tools  # contains operators
-from deap import algorithms  # contains ready genetic evolutionary loops
 from scipy import signal
 
-import devices
-import step_info
+from step_info import StepInfo
 
 
 def find_x_init(trans_func):
@@ -67,8 +66,9 @@ def simulation_fitness(U, T, X0, trans_func):
         return (1000.0,)
     else:
         (_, yout, _) = signal.lsim2(trans_func, U=U, T=T, X0=X0, atol=1e-12)
-        t = rise_time(T, yout)
-        mse = mean_squared_error(yout, 110, 240)
+        si = StepInfo(yout, T, yout[0], yout[-1])
+        t = si.rise_time
+        mse = si.mse
         return (mse,)
 
 
@@ -247,76 +247,6 @@ def eaSimple(
     return population, logbook
 
 
-def main_optimizer(optimizing, range_):
-    default_args = {
-        "pop_size": 60,
-        "mu": 0,
-        "sigma": 0.15,
-        "indpb": 0.06,
-        "tournsize": 4,
-        "cxpb": 0.9,
-        "mutpb": 0.3,
-    }
-    tot_evals = 80000
-
-    pop_size = default_args["pop_size"]
-    mu = default_args["mu"]
-    sigma = default_args["sigma"]
-    indpb = default_args["indpb"]
-    tournsize = default_args["tournsize"]
-    cxpb = default_args["cxpb"]
-    mutpb = default_args["mutpb"]
-
-    for i in range_:
-        random.seed(0)
-        if optimizing == "pop_size":
-            pop_size = i
-        elif optimizing == "mu":
-            mu = i
-        elif optimizing == "sigma":
-            sigma = i
-        elif optimizing == "indpb":
-            indpb = i
-        elif optimizing == "tournsize":
-            tournsize = i
-        elif optimizing == "cxpb":
-            cxpb = i
-        elif optimizing == "mutpb":
-            mutpb = i
-
-        ngen = tot_evals // pop_size
-        print(
-            "optimizing {}, pop_size {}, mu {}, sigma {}, indpb {}, tournsize {}, cxpb {}, mutpb {}, ngen {}.png".format(
-                optimizing, pop_size, mu, sigma, indpb, tournsize, cxpb, mutpb, ngen
-            )
-        )
-
-        x = SimulationOptimization(
-            pop_size=pop_size,
-            mu=mu,
-            sigma=sigma,
-            indpb=indpb,
-            tournsize=tournsize,
-            cxpb=cxpb,
-            mutpb=mutpb,
-            ngen=ngen,
-            interactive=False,
-            show_plotting=False,
-        )
-        x.run(show_final_plot=True)
-        plt.ylim((0.9, 1.11))
-        plt.pause(0.5)
-        plt.savefig(
-            "optimizing {}, pop_size {}, mu {}, sigma {}, indpb {}, tournsize {}, cxpb {}, mutpb {}, ngen {}.png".format(
-                optimizing, pop_size, mu, sigma, indpb, tournsize, cxpb, mutpb, ngen
-            )
-        )
-        plt.pause(0.5)
-        plt.close()
-        plt.pause(0.5)
-        del x
-
-
 class SimulationOptimization:
     def __init__(
         self,
@@ -422,219 +352,83 @@ class SimulationOptimization:
             plt.pause(0.05)
 
 
-class SOAOptimization:
-    def __init__(
-        self,
-        pop_size=120,
-        mu=0,
-        sigma=0.15,
-        indpb=0.06,
-        tournsize=4,
-        cxpb=0.9,
-        mutpb=0.45,
-        ngen=2000,
-        interactive=True,
-        show_plotting=True,
-    ):
-        """Implements optimization for real SOA.
+def hyperparameter_optimization(optimizing, range_):
+    """Performs hyperparameter optimization on the simulation.
 
-        Args:
-            pop_size (int): Populaiton size (number of individuals in
-                each generation).
-            mu (number): Mean for the gaussian addition mutation.
-            sigma (number): Standard deviation for the gaussian addition
-                mutation.
-            indpb (number): Independent probability for each attribute
-                to be mutated.
-            tournsize (int): The number of individuals participating in
-                each tournament.
-            cxpb (number): The probability of mating two individuals.
-            mutpb (number): The probability of mutating an individual.
-            ngen (int): The number of generation.
-            interactive (bool): Allows to press 'q' to stop execution,
-                requires confirmation after finished run.
-            show_plotting (bool): Shows a live plot of progress.
-        """
-        self.awg = devices.TektronixAWG7122B("GPIB1::1::INSTR")
-        self.osc = devices.Agilent86100C("GPIB1::7::INSTR")
+    Args:
+        optimizing (str): Name of parameter to be optimized.
+        range_ (Iterable(number)): Values to be tried for the
+            optimization.
+    """
+    default_args = {
+        "pop_size": 60,
+        "mu": 0,
+        "sigma": 0.15,
+        "indpb": 0.06,
+        "tournsize": 4,
+        "cxpb": 0.9,
+        "mutpb": 0.3,
+    }
+    tot_evals = 80000
 
-        # setup oscilloscope for measurement
-        self.osc.set_acquire(average=True, count=30, points=1350)
-        self.osc.set_timebase(position=2.4e-8, range_=30e-9)
-        self.T = np.linspace(start=0, stop=30e-9, num=1350)
+    pop_size = default_args["pop_size"]
+    mu = default_args["mu"]
+    sigma = default_args["sigma"]
+    indpb = default_args["indpb"]
+    tournsize = default_args["tournsize"]
+    cxpb = default_args["cxpb"]
+    mutpb = default_args["mutpb"]
 
-        # find rise-time ref values
-        self.osc.set_timebase(position=2.4e-8, range_=15e-9)
-        self.awg.send_waveform([-0.5] * 120 + [0.5] * 120)
-        time.sleep(5)
-        res = self.osc.measurement(1)
-        self.osc.set_timebase(position=2.4e-8, range_=30e-9)
-        self.ss_low = res[0]
-        self.ss_high = res[-1]
+    for i in range_:
+        random.seed(0)
+        if optimizing == "pop_size":
+            pop_size = i
+        elif optimizing == "mu":
+            mu = i
+        elif optimizing == "sigma":
+            sigma = i
+        elif optimizing == "indpb":
+            indpb = i
+        elif optimizing == "tournsize":
+            tournsize = i
+        elif optimizing == "cxpb":
+            cxpb = i
+        elif optimizing == "mutpb":
+            mutpb = i
 
-        creator.create("Fitness", base.Fitness, weights=(-1.0,))
-        creator.create("Individual", list, fitness=creator.Fitness)
-
-        self.toolbox = base.Toolbox()
-        initial = lambda: [random.uniform(-1, 0) for _ in range(30)] + [
-            random.uniform(0, 1) for _ in range(30)
-        ]
-        # fmt: off
-        self.toolbox.register("ind", tools.initIterate, creator.Individual, initial)
-        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.ind, n=pop_size)
-        self.toolbox.register("evaluate", self.SOA_fitness)
-        self.toolbox.register("mate", tools.cxTwoPoint)
-        self.toolbox.register("mutate", tools.mutGaussian, mu=mu, sigma=sigma, indpb=indpb)
-        self.toolbox.register("select", tools.selTournament, tournsize=tournsize)
-        self.toolbox.register("eaSimple", eaSimple, cxpb=cxpb, mutpb=mutpb, ngen=ngen, interactive=interactive, show_plotting=show_plotting)
-        # fmt: on
-
-    def valid_U(self, U):
-        """Checks if the driving signal is valid.
-
-        Args:
-            U (List[float]): Driving signal. Length is assumed to be 60,
-                and each point must be strongly between -1.0 and 1.0.
-                The rising edge must be in the middle.
-
-        Returns:
-            bool: True is driving signal is valid, False otherwise.
-        """
-        return (
-            all(i > -1.0 for i in U)
-            and all(i < 1.0 for i in U)
-            and all(i < 0.0 for i in U[28:30])
-            and all(i > 0.0 for i in U[30:32])
-        )
-
-    def SOA_fitness(self, U):
-        if not self.valid_U(U):
-            return (1000.0,)
-        else:
-            expanded_U = [-0.5] * 90 + list(U) + [0.5] * 90
-            self.awg.send_waveform(expanded_U, suppress_messages=True)
-            time.sleep(5)
-            result = self.osc.measurement(channel=1)
-            step_info.StepInfo(result)
-            return (settling_time(self.T, result, self.ss_low, self.ss_high, 0.05),)
-
-    def run(self, show_final_plot=True):
-        """Runs the optimization.
-
-        Args:
-            show_final_plot (bool): If True, will show a plot with
-                the fitness over the generations.
-        """
-        self.pop = self.toolbox.population()
-        self.hof = tools.HallOfFame(1)
-        self.stats = tools.Statistics()
-        self.stats.register("min_per_population", best_of_population)
-        self.stats.register(
-            "min_fitness", lambda pop: np.min([ind.fitness.values for ind in pop])
-        )
-
-        self.pop, self.logbook = self.toolbox.eaSimple(
-            self.pop, self.toolbox, stats=self.stats, halloffame=self.hof
-        )
-
+        ngen = tot_evals // pop_size
         print(
-            "Best individual is: {}\nwith fitness: {}".format(
-                self.hof[0], self.hof[0].fitness
+            "optimizing {}, pop_size {}, mu {}, sigma {}, indpb {}, tournsize {}, cxpb {}, mutpb {}, ngen {}.png".format(
+                optimizing, pop_size, mu, sigma, indpb, tournsize, cxpb, mutpb, ngen
             )
         )
 
-        gen, min_, = self.logbook.select("gen", "min_fitness")
-        if show_final_plot:
-            plt.figure()
-            plt.plot(gen, min_, label="minimum")
-            plt.xlabel("Generation")
-            plt.ylabel("Fitness")
-            plt.legend(loc="lower right")
-            plt.show()
-            plt.pause(0.05)
-
-
-def rising_edge_optimization():
-    """Finds optimal rising edge.
-
-    The function tests out different rising edges, as follows:
-    each full wave is 240 points, first 80 are -0.75, last 80 are 0.75,
-    and the points in between are either 0.75 or 1.0 (negative before
-    the rising edge). Then rise times can be compared.
-    """
-    awg = devices.TektronixAWG7122B("GPIB1::1::INSTR")
-    osc = devices.Agilent86100C("GPIB1::7::INSTR")
-
-    # setup oscilloscope for measurement
-    osc.set_acquire(average=True, count=30, points=1350)
-    osc.set_timebase(position=4e-8, range_=12e-9)
-    T = np.linspace(start=0, stop=12e-9, num=1350)
-
-    # find rise-time ref values
-    awg.send_waveform([-0.75] * 120 + [0.75] * 120, suppress_messages=True)
-    time.sleep(5)
-    res = osc.measurement(channel=1)
-    rise_start = res[0]
-    rise_end = res[-1]
-
-    results = []
-
-    for n_high in range(9):
-        for n_low in range(9):
-            rising_edge = ([-0.75] * 5 * (8 - n_low) + [-1.0] * 5 * n_low) + (
-                [1.0] * 5 * n_high + [0.75] * 5 * (8 - n_high)
-            )
-            awg.send_waveform(
-                [-0.75] * 80 + rising_edge + [0.75] * 80, suppress_messages=True
-            )
-            time.sleep(5)
-            result = osc.measurement(channel=1)
-            my_rise_time = rise_time(
-                T, result, rise_start=rise_start, rise_end=rise_end
-            )
-            print(my_rise_time)
-            results.append((rising_edge, result, my_rise_time))
-
-    return results
-
-
-def steady_state_optimization():
-    """Compares the effect of different steady-state on the rise time.
-    """
-    awg = devices.TektronixAWG7122B("GPIB1::1::INSTR")
-    osc = devices.Agilent86100C("GPIB1::7::INSTR")
-
-    # setup oscilloscope for measurement
-    osc.set_acquire(average=True, count=30, points=1350)
-    osc.set_timebase(position=4e-8, range_=12e-9)
-    T = np.linspace(start=0, stop=12e-9, num=1350)
-
-    results = []
-
-    for level in [1.0, 0.9, 0.8, 0.7, 0.6, 0.5]:
-        awg.send_waveform([-level] * 120 + [level] * 120, suppress_messages=True)
-        time.sleep(5)
-        res = osc.measurement(channel=1)
-        rise_start = res[0]
-        rise_end = res[-1]
-        rise_time_pure = rise_time(T, res, rise_start=rise_start, rise_end=rise_end)
-
-        awg.send_waveform(
-            [-level] * 110 + [-1.0] * 10 + [1.0] * 10 + [level] * 110,
-            suppress_messages=True,
+        x = SimulationOptimization(
+            pop_size=pop_size,
+            mu=mu,
+            sigma=sigma,
+            indpb=indpb,
+            tournsize=tournsize,
+            cxpb=cxpb,
+            mutpb=mutpb,
+            ngen=ngen,
+            interactive=False,
+            show_plotting=False,
         )
-        time.sleep(5)
-        res = osc.measurement(channel=1)
-        rise_time_optimized = rise_time(
-            T, res, rise_start=rise_start, rise_end=rise_end
+        x.run(show_final_plot=True)
+        plt.ylim((0.9, 1.11))
+        plt.pause(0.5)
+        plt.savefig(
+            "optimizing {}, pop_size {}, mu {}, sigma {}, indpb {}, tournsize {}, cxpb {}, mutpb {}, ngen {}.png".format(
+                optimizing, pop_size, mu, sigma, indpb, tournsize, cxpb, mutpb, ngen
+            )
         )
-
-        results.append((level, rise_time_pure, rise_time_optimized))
-
-    return results
-
+        plt.pause(0.5)
+        plt.close()
+        plt.pause(0.5)
+        del x
 
 
 if __name__ == "__main__":
-    x = SOAOptimization()
+    x = SimulationOptimization()
     x.run()
