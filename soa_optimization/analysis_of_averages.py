24c67f368aef754db3b513abad4df0aeb8d88b90
Konrad Chlupka <chlupkaw@gmail.com>
2019-12-11 14:11:56 +0000

    Put analysis_of_averages code into a func

--- soa_optimization/analysis_of_averages.py
+++ soa_optimization/analysis_of_averages.py
@@ -1,15 +1,19 @@
 import devices
 import time
 
-results = []
-osc = devices.Agilent86100C("GPIB1::7::INSTR")
 
-for n_of_averages in range(1, 101):
-    osc.set_acquire(count=n_of_averages)
-    dict_resp = {}
-    dict_resp["n_of_averages"] = n_of_averages
-    dict_resp["outputs"] = []
-    for i in range(100):
-        time.sleep(0.5 + n_of_averages / 10)
-        dict_resp["outputs"].append(osc.measurement(channel=1))
-    results.append(dict_resp)
+def analysis_of_averages():
+    results = []
+    osc = devices.Agilent86100C("GPIB1::7::INSTR")
+
+    for n_of_averages in range(1, 101):
+        osc.set_acquire(count=n_of_averages)
+        dict_resp = {}
+        dict_resp["n_of_averages"] = n_of_averages
+        dict_resp["outputs"] = []
+        for i in range(100):
+            time.sleep(0.5 + n_of_averages / 10)
+            dict_resp["outputs"].append(osc.measurement(channel=1))
+        results.append(dict_resp)
+
+    return results
